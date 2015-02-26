a58222b6b029de599ccc55ffc38ca5f04b38831e
ilboud <abdallah.atie@gmail.com>
2015-02-26 18:11:41 +0400

    Front page HTML noe generated from template

--- functional_tests.py
+++ functional_tests.py
@@ -24,8 +24,8 @@ class NewVisitorTest(unittest.TestCase):
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
-        elf.assertEqual(
-            inputbox.geet_attribute('placeholder'),
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
         )   
 
@@ -36,12 +36,13 @@ class NewVisitorTest(unittest.TestCase):
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
-        inputbox.send_keys(keys.ENTER)
+        inputbox.send_keys(Keys.ENTER)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
+            any(row.text == '1: Buy peacock feathers' for row in rows), 
+            "New to-do item did not appear in table"
         )
 
 # There is still a text box inviting her to add another item. She
--- lists/tests.py
+++ lists/tests.py
@@ -13,6 +13,6 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_tp_string('home.html')
+        expected_html = render_to_string('home.html')
         self.assertEqual(response.content.decode(), expected_html)
         
